ab18987468e3082cccb328794191abb25a4e4fcf
SEARA\liviu <learmeanu@gmail.com>
2023-12-25 21:28:11 +1300

    WV - EOD

--- sg_settings.py
+++ sg_settings.py
@@ -20,7 +20,11 @@ init_file_header_template = Template('''
 
 enum_template = Template('''
 class $enum_name(Enum):
-    # isEnum : $enum_type                         
+    """
+    #: isEnum : $enum_type
+
+$enum_doc   
+    """
     $enum_body
 ''')
 
